be88b59a67d96a46f4a162890baf34b90da9773e
IT_page <IT_page@DESKTOP-ES5NIT9>
2018-08-07 11:20:49 +0800

    添加项目文件。

---
+++ Storage/Areas/Admin/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Storage.Areas.Admin.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Admin/Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Pro()
+        {
+            return View();
+        }
+    }
+}
